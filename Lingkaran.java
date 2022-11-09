da29e5cb345e3a55554c87ad4da00b569fe842ae
demskii <109432905+demskii@users.noreply.github.com>
2022-11-10 00:15:01 +0700

    Add files via upload

---
+++ Lingkaran.java
@@ -0,0 +1,20 @@
+public class Lingkaran extends BangunDatar {
+    public double jariJari;
+
+    public Lingkaran(double jariJari) {
+        this.jariJari = jariJari;
+    }
+
+    public void hitungLuas() {
+        luas = 3.14 * jariJari * jariJari;
+    }
+
+    public void hitungKeliling() {
+        keliling = 2 * 3.14 * jariJari;
+    }
+
+    public void tampilData() {
+        System.out.println("JARI-JARI = " + jariJari);
+        super.tampilData();
+    }
+}
