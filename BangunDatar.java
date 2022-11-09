9d406730eabf618737f75c52961c70ae92c5a68a
demskii <109432905+demskii@users.noreply.github.com>
2022-11-10 00:14:21 +0700

    Create BangunDatar.java

---
+++ BangunDatar.java
@@ -0,0 +1,18 @@
+public class BangunDatar {
+    public double luas, keliling;
+
+    public void hitungLuas() {
+        luas = 0;
+    }
+
+    public void hitungKeliling() {
+        keliling = 0;
+    }
+
+    public void tampilData() {
+        System.out.println("LUAS = " + luas);
+        System.out.println("KELILING= " + keliling);
+
+    }
+
+}
